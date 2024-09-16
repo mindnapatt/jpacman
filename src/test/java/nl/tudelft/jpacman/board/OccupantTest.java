368ddd571e8e3882c5a16bce1245a92a3a028c01
mindnapatt <131891751+mindnapatt@users.noreply.github.com>
2024-09-16 21:41:49 +0700

    6488001 Napat Boongaree

--- src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
+++ src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
@@ -29,6 +29,7 @@ class OccupantTest {
     /**
      * Asserts that a unit has no square to start with.
      */
+
     @Test
     void noStartSquare() {
         // Remove the following placeholder:
@@ -41,8 +42,10 @@ class OccupantTest {
      */
     @Test
     void testOccupy() {
-        // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        Square square = new BasicSquare();
+        unit.occupy(square);
+        assertThat(unit.getSquare()).isEqualTo(square);
+        assertThat(square.getOccupants()).contains(unit);
     }
 
     /**
@@ -51,7 +54,14 @@ class OccupantTest {
      */
     @Test
     void testReoccupy() {
-        // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        Square firstSquare = new BasicSquare();
+        Square secondSquare = new BasicSquare();
+
+        unit.occupy(firstSquare);
+        unit.occupy(secondSquare);
+
+        assertThat(unit.getSquare()).isEqualTo(secondSquare);
+        assertThat(secondSquare.getOccupants()).contains(unit);
+        assertThat(firstSquare.getOccupants()).doesNotContain(unit);
     }
 }
