077c85e491dd3180019919c238f0b3abdb25d786
mindnapatt <131891751+mindnapatt@users.noreply.github.com>
2024-11-05 12:16:24 +0700

    update BoardTest2

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 public class BoardTest {
 
     @Test
